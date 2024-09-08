ca272ba47d7f3ec58b00616c485e49abe29a2286
obre-bjorn <obre.bjorn@gmail.com>
2024-09-09 00:15:22 +0300

    Initialized project and setup database together with prisma ORM

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const session = require('express-session')
+
+
+
+const app = express()
+
+
+
+
+app.listen(3000, () =>{
+    return `App running on Port:3000`
+})
